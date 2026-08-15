94cad1f597355ff812e7c5ac8a36af95a8847b79
Muhammad Huzaifa Khan <huzaifakhank778@gmail.com>
2026-08-15 11:58:23 +0500

    Update frontend API URL

--- frontend/src/services/studentService.js
+++ frontend/src/services/studentService.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "https://student-management-system-two-jet.vercel.app";
 
 export const getStudents = async () => {
   const response = await fetch(`${API_URL}/students`);
